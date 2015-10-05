611c25204c826220687ca0e7da3a090397b4c1fa
Levi Lovelock <lovelock.levi@gmail.com>
2015-10-05 18:09:17 +1100

    Removed docs

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	_ "github.com/astaxie/beego/session/mysql"
-	_ "github.com/levilovelock/magitrak/docs"
 	_ "github.com/levilovelock/magitrak/routers"
 
 	"github.com/astaxie/beego"
