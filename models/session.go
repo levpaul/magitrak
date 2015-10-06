c85642504b02f38adf7bd15d708c084716f25f98
Levi Lovelock <lovelock.levi@gmail.com>
2015-10-06 17:54:16 +1100

    Removed unused auth bypass features

--- models/session.go
+++ models/session.go
@@ -1,10 +1,6 @@
 package models
 
-import (
-	"os"
-
-	"github.com/astaxie/beego/context"
-)
+import "github.com/astaxie/beego/context"
 
 const (
 	SESSION_NAME = "magitrak"
@@ -16,12 +12,6 @@ type MagiSession struct {
 }
 
 func IsLoggedInFilter(c *context.Context) {
-	// TODO: Fix this workaround with appconfig (mainly for testing)
-	authRequired := os.Getenv("sessionAuthRequired")
-	if authRequired == "false" {
-		return
-	}
-
 	sessionIntf := c.Input.Session(SESSION_NAME)
 	if sessionIntf == nil || !sessionIntf.(MagiSession).Authenticated || sessionIntf.(MagiSession).UserId == 0 {
 		c.Redirect(302, "/v1/auth/unauthorised")
